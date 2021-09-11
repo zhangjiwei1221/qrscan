07a7f2495525b9f626a5353d3536ce00298c7cba
zhangjiwei <1945192314@qq.com>
2021-09-11 11:01:30 +0800

    back init

---
+++ qrscan-back/src/main/java/cn/butterfly/qrscan/QrscanBackApplication.java
@@ -0,0 +1,13 @@
+package cn.butterfly.qrscan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrscanBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrscanBackApplication.class, args);
+    }
+
+}
