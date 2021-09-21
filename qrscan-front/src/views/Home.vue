75f374c378cf84b8da2d0db77f5666283eab6dcb
zhangjiwei <1945192314@qq.com>
2021-09-21 17:14:32 +0800

    改进版本完成

--- qrscan-front/src/views/Home.vue
+++ qrscan-front/src/views/Home.vue
@@ -1,12 +1,34 @@
 <template>
   <div class="home">
     <img src="../assets/img/bg.jpg"/>
+    <el-button
+      type="danger"
+      class="logout"
+      @click="logout"
+      plain
+    >注销</el-button>
   </div>
 </template>
 
 <script>
+import {
+  Button,
+  Message
+} from 'element-ui'
+import store from "@/store";
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    'el-button': Button
+  },
+  methods: {
+    logout() {
+      store.commit('removeToken')
+      this.$router.push('/login')
+      Message.success('注销成功')
+    }
+  }
 }
 </script>
 
@@ -15,5 +37,14 @@ export default {
   width: 100%;
   text-align: center;
   margin-top: calc(50vh - 386px);
+}
+
+.logout {
+  position: absolute;
+  top: 10px;
+  right: 50px;
+  width: 90px;
+  height: 32px;
+  line-height: 10px;
 }
 </style>
